c4718943086c429f8e97b4e645d2c93128ed891c
33 <yuluolfs@163.com>
2025-04-08 13:48:37 +0800

    只保留win平台，改用MT编译

--- vcpkg-overlay/triplets/maa-x86-windows.cmake
+++ vcpkg-overlay/triplets/maa-x86-windows.cmake
@@ -1,4 +1,4 @@
-set(VCPKG_TARGET_ARCHITECTURE arm64)
-set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_TARGET_ARCHITECTURE x86)
+set(VCPKG_CRT_LINKAGE static)
 
 include(${CMAKE_CURRENT_LIST_DIR}/maa-windows-library-override.cmake)
